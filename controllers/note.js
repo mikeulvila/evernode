74ad2b1d4a08b2095be8b38b259b65a850e0223b
Mike Ulvila <mikeulvila@me.com>
2016-02-18 15:08:35 -0600

    refactored note ctrl to es6 syntax

--- controllers/note.js
+++ controllers/note.js
@@ -1,73 +1,75 @@
 'use strict'
 const Note = require('../models/note.js');
 
-// get all notes
-module.exports.index = (req, res) => {
-  Note.find({}, (err, notes) => {
-    if (err) throw err;
-    res.render('notes-index', {
-      notes: notes
-    });
-  }).sort({title: 1});
-};
+module.exports = {
+  // get all notes
+  index (req, res) {
+    Note.find({}, (err, notes) => {
+      if (err) throw err;
+      res.render('notes-index', {
+        notes: notes
+      });
+    }).sort({title: 1});
+  },
 
-// new note form
-module.exports.newNote = (req, res) => {
-  res.render('new-note', {
-    title: 'New Note'
-  })
-};
-
-// show note
-module.exports.show = (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
-    res.render('show-note', {
-      note: note
-    });
-  })
-};
-
-// create note
-module.exports.create = (req, res) => {
-  Note.create(req.body, (err, note) => {
-    if (err) throw err;
-    console.log('note: ', note);
-    res.redirect(`/notes/${note._id}`);
-  });
-};
-
-// delete note
-module.exports.destroy = (req, res) => {
-  Note.findByIdAndRemove(req.params.id, (err) => {
-    if (err) throw err;
-
-    res.redirect('/notes');
-  });
-};
-
-// edit note
-module.exports.edit = (req, res) => {
-  Note.findById(req.params.id, (err, note) => {
-    if (err) throw err;
-
+  // new note form
+  newNote (req, res) {
     res.render('new-note', {
-      note: note
-    });
+      title: 'New Note'
+    })
+  },
 
-  });
-};
-
-// update note
-module.exports.update = (req, res) => {
-  Note.findByIdAndUpdate(req.params.id,
-    req.body, (err, note) => {
+  // show note
+  show (req, res) {
+    Note.findById(req.params.id, (err, note) => {
       if (err) throw err;
 
+      res.render('show-note', {
+        note: note
+      });
+    })
+  },
+
+  // create note
+  create (req, res) {
+    Note.create(req.body, (err, note) => {
+      if (err) throw err;
+      console.log('note: ', note);
       res.redirect(`/notes/${note._id}`);
-    }
-  );
+    });
+  },
+
+  // delete note
+  destroy (req, res) {
+    Note.findByIdAndRemove(req.params.id, (err) => {
+      if (err) throw err;
+
+      res.redirect('/notes');
+    });
+  },
+
+  // edit note
+  edit (req, res) {
+    Note.findById(req.params.id, (err, note) => {
+      if (err) throw err;
+
+      res.render('new-note', {
+        note: note
+      });
+
+    });
+  },
+
+  // update note
+  update (req, res) {
+    Note.findByIdAndUpdate(req.params.id,
+      req.body, (err, note) => {
+        if (err) throw err;
+
+        res.redirect(`/notes/${note._id}`);
+      }
+    );
+  }
 };
 
 
