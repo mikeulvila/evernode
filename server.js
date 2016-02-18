3ec60a2bb67a9b508167d9d2979ba4fc10f1d945
Mike Ulvila <mikeulvila@me.com>
2016-02-18 10:36:45 -0600

    new note

--- server.js
+++ server.js
@@ -1,23 +1,40 @@
 'use strict'
 
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const bodyParser = require('body-parser');
 
-// mongodb connection
-//const MongoClient = require('mongodb').MongoClient
-const mongoose = require('mongoose');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
 //const routes = require('./routes/index.js');
 
-
 // get root route
 app.get('/', (req, res) => {
   res.send('Server Running');
+});
+
+// GET new note form
+app.get('/notes/new', (req, res) => {
+  res.render('new-note')
+});
+
+// POST new note
+app.post('/notes', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+
 });
 
 //server listening on port..
